61718a072d1ea6e4f97a693ebcc12c6715ea305b
YuriRibeiro <yribeiro@poli.ufrj.br>
2021-03-06 20:27:31 -0300

    Adding exp logs and some fixes

--- RedesNeurais/YOLOv3_UAVDT_6/upload_to_bucket_YOLOv3_UAVDT_6.sh
+++ RedesNeurais/YOLOv3_UAVDT_6/upload_to_bucket_YOLOv3_UAVDT_6.sh
@@ -4,4 +4,4 @@
 #logs/*.txt
 !gsutil -m cp -r logs/*.txt gs://cfdy/Experimentos_logs_txt/YOLOv3_UAVDT_6/logs
 #train files
-!gsutil -m cp -r /home/yuri/Desktop/cfd/RedesNeurais/YOLOv3_UAVDT_6/YOLOv3_UAVDT_train/YOLOv3_UAVDT_6-04_Mar_2021_16h_41m/ gs://cfdy/Experimentos/YOLOv3_UAVDT_6/YOLOv3_UAVDT_6-04_Mar_2021_16h_41m
+!gsutil -m cp -r /home/yuri/Desktop/cfd/RedesNeurais/YOLOv3_UAVDT_6/YOLOv3_UAVDT_train/YOLOv3_UAVDT_6-04_Mar_2021_16h_41m/ gs://cfdy/Experimentos/YOLOv3_UAVDT_6/YOLOv3_UAVDT_train/YOLOv3_UAVDT_6-04_Mar_2021_16h_41m
--- RedesNeurais/YOLOv3_UAVDT_8/upload_to_bucket_YOLOv3_UAVDT_8.sh
+++ RedesNeurais/YOLOv3_UAVDT_8/upload_to_bucket_YOLOv3_UAVDT_8.sh
@@ -4,4 +4,4 @@
 #logs/*.txt
 !gsutil -m cp -r logs/*.txt gs://cfdy/Experimentos_logs_txt/YOLOv3_UAVDT_8/logs
 #train files
-!gsutil -m cp -r /home/yuri/Desktop/cfd/RedesNeurais/YOLOv3_UAVDT_8/YOLOv3_UAVDT_train/YOLOv3_UAVDT_8-04_Mar_2021_18h_12m/ gs://cfdy/Experimentos/YOLOv3_UAVDT_8/YOLOv3_UAVDT_8-04_Mar_2021_18h_12m
+!gsutil -m cp -r /home/yuri/Desktop/cfd/RedesNeurais/YOLOv3_UAVDT_8/YOLOv3_UAVDT_train/YOLOv3_UAVDT_8-04_Mar_2021_18h_12m/ gs://cfdy/Experimentos/YOLOv3_UAVDT_8/YOLOv3_UAVDT_train/YOLOv3_UAVDT_8-04_Mar_2021_18h_12m
